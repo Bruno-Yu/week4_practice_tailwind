d8388e204b324b3c5ea48c5356d994f26fdf447c
Bruno Yu <jackhellowin@gmail.com>
2022-08-04 21:29:35 +0800

    Update 2022-08-04T13:29:25.208Z

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// console.log('Hello!');
+// $(document).ready(() => {
+//   console.log('HesSchool Hello!');
+// });
+$(function () {
+  $('.js-menu-btn').on('click', function (e) {
+    e.preventDefault;
+    $('.js-menu').slideToggle();
+  });
+});
+//# sourceMappingURL=all.js.map
